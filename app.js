a7bfea0fb521f4e24e723b447638fc46763c65bf
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-11-27 13:57:43 -0700

    queries built and used.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+const queries = require('./queries')
 
 const port = process.env.PORT || 3000
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-app.get('/', (req, res, next) => {
-    res.send('this worked')
+app.get('/', (req, res) => {
+    queries.listAll().then(students => res.send(students))
 })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))

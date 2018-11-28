7410258e74df8dce24ca819e34aa6dd61b174db5
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-11-28 10:36:58 -0700

    body parser and cors installed

--- app.js
+++ app.js
@@ -9,4 +9,12 @@ app.get('/', (req, res) => {
     queries.listAll().then(students => res.send(students))
 })
 
+app.get('/:id', (req, res) => {
+    queries.getById2(req.params.id).then(students => res.send(students))
+})
+
+app.post('/q', (req, res) =>{
+    queries.createStudent(req.body).then(students => res.send(students))
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- queries.js
+++ queries.js
@@ -3,5 +3,14 @@ const database = require('./database-connection')
 module.exports = {
     listAll(){
         return database('students')
+    },
+    getById(id){
+        return database('students').where('id', id)
+    },
+    getById2(id){
+        return database('students').where({ id : id}).first()
+    },
+    createStudent(newStudent){
+        return database('students').insert(newStudent).returning('*')
     }
 }

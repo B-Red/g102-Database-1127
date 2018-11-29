cd7f6590ae793b11ae4c656fce96031158bcf019
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-11-29 09:58:46 -0700

    delete route added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const bodyParser = require("body-parser")
+const cors = require('cors')
 const app = express()
 const queries = require('./queries')
 
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+app.use(cors())
+
 
 
 app.get('/', (req, res) => {
@@ -13,8 +19,12 @@ app.get('/:id', (req, res) => {
     queries.getById2(req.params.id).then(students => res.send(students))
 })
 
-app.post('/q', (req, res) =>{
+app.post('/', (req, res) =>{
     queries.createStudent(req.body).then(students => res.send(students))
+})
+
+app.delete('/:id', (req,res) => {
+    queries.deleteStudent(req.params.id).then(res.send("Status:204, Yooo you deleted that fool."))
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
